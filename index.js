d90298f9983a16a129ce4ce7dec61cc8327ac9bc
iteacat <justwy@gmail.com>
2016-12-03 18:42:12 -0500

    add another button

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ var DashButton = require('dash-button');
 var hueInterface = require('./hueInterface')
 
 const CHARMIN_DASH_BUTTON_MAC_ADDRESS = 'ac:63:be:9f:df:9c';
+const SMART_WATER_DASH_BUTTON_MAC_ADDRESS = '50:f5:da:d2:20:1e';
 
 var charminButton = new DashButton(CHARMIN_DASH_BUTTON_MAC_ADDRESS);
+var smartWaterButton = new DashButton(SMART_WATER_DASH_BUTTON_MAC_ADDRESS);
 
-var count = 0;
+var charminButtonCount = 0;
 
 charminButton.addListener(function() {
     hueInterface.toggleLightsWithMaxBright(hueInterface.GROUPS.BEDROOM_GROUP_ID, function(err) {
-        console.log("Charmin button pressed %d times", ++count, err);
+        console.log("Charmin button pressed %d times", ++charminButtonCount, err);
+    })
+});
+
+var smartWaterButtonCount = 0;
+smartWaterButton.addListener(function() {
+    hueInterface.toggleLightsWithMaxBright(hueInterface.GROUPS.LIVING_GROUP_ID, function(err) {
+        console.log("Smart Water button pressed %d times", ++smartWaterButtonCount, err);
     })
 });
